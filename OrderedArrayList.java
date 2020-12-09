054b351bf9db6b0384860589b3c2c2a3ed11a833
J3rry-L <jliang21@stuy.edu>
2020-12-09 17:53:59 -0500

    Created helper function findIndex

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,4 +5,14 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int initialCapacity){
     super(initialCapacity);
   }
+  private int findIndex(T element){
+    int index = 0;
+    while(element.compareTo(get(index + 1)) > 0){
+      index++;
+    }
+    return index;
+  }
+  public boolean add(T element){
+    return false;
+  }
 }
