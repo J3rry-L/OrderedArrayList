4b6cd1a00a26abcb68c8e5299217eb080fde4d2b
J3rry-L <jliang21@stuy.edu>
2020-12-09 13:22:19 -0500

    Overrode set method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,4 +6,12 @@ public class NoNullArrayList<T> extends ArrayList<T>{
   public NoNullArrayList(int initialCapacity){
     super(initialCapacity);
   }
+  public T set(int index, T element){
+    if (element == null){
+      throw new IllegalArgumentException("null is not valid element");
+    }
+    else{
+      return(super.set(index, element));
+    }
+  }
 }
