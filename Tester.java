f3f9719195594d3fd21e992d2af1341058242ced
J3rry-L <jliang21@stuy.edu>
2020-12-10 20:09:16 -0500

    Added test cases

--- Tester.java
+++ Tester.java
@@ -1,6 +1,6 @@
 public class Tester{
   public static void main(String[] args){
-    NoNullArrayList expectNullError = new NoNullArrayList();
+    NoNullArrayList<String> expectNullError = new NoNullArrayList<String>();
     expectNullError.add("bob");
     System.out.println(expectNullError);
     try{
@@ -9,5 +9,39 @@ public class Tester{
     catch(IllegalArgumentException e){
           System.out.println("IllegalArgumentException");
     }
+    NoNullArrayList<Integer> numbers = new NoNullArrayList<Integer>();
+    System.out.println(numbers.add((Integer) 111));
+    System.out.println(numbers);
+    System.out.println(numbers.add((Integer) 11));
+    System.out.println(numbers);
+    System.out.println(numbers.add((Integer) 1111));
+    System.out.println(numbers);
+    numbers.add(2, (Integer) 111);
+    System.out.println(numbers);
+    System.out.println(numbers.set(0, (Integer) 1));
+    System.out.println(numbers);
+
+    OrderedArrayList<Integer> A = new OrderedArrayList<Integer>();
+    A.add(12, (Integer)90);
+    System.out.println(A);
+    System.out.println(A.add((Integer)100));
+    System.out.println(A);
+    System.out.println(A.add((Integer)101));
+    System.out.println(A);
+    System.out.println(A.set(2, (Integer) 85));
+    System.out.println(A);
+    OrderedArrayList<String> B = new OrderedArrayList<String>();
+    System.out.println(B.add("apple"));
+    System.out.println(B);
+    B.add(35, "banana");
+    System.out.println(B);
+    System.out.println(B.add("cantaloupe"));
+    System.out.println(B);
+    System.out.println(B.add("fig"));
+    System.out.println(B);
+    System.out.println(B.add("elderberry"));
+    System.out.println(B);
+    System.out.println(B.add("dates"));
+    System.out.println(B);
   }
 }
