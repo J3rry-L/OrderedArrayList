ac189e8474c88fbabfd0d5d96c226ebca28da759
J3rry-L <jliang21@stuy.edu>
2020-12-10 19:28:44 -0500

    Did preliminary testing

--- Tester.java
+++ Tester.java
@@ -1,5 +1,13 @@
 public class Tester{
   public static void main(String[] args){
-    
+    NoNullArrayList expectNullError = new NoNullArrayList();
+    expectNullError.add("bob");
+    System.out.println(expectNullError);
+    try{
+      expectNullError.add(null);
+    }
+    catch(IllegalArgumentException e){
+          System.out.println("IllegalArgumentException");
+    }
   }
 }
